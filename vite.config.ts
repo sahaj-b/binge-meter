0d037c178c6867e845aa12fc6db3a34e5c284993
sahaj-b <sahajb0606@gmail.com>
2025-06-08 20:27:54 +0530

    fix vite and add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import manifest from "./manifest.json";
+import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        // define every non-JS/TS entry point
-        options: resolve(__dirname, "src/options/options.html"),
-        popup: resolve(__dirname, "src/popup/popup.html"),
-        // define scripts that are not linked in any HTML.
-        background: resolve(__dirname, "src/background.ts"),
-        content: resolve(__dirname, "src/content.ts"),
-      },
-      output: {
-        entryFileNames: `[name].js`,
-      },
-    },
-  },
+  plugins: [react(), tailwindcss(), crx({ manifest })],
 });
