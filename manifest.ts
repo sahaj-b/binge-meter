e2d832ffa805b10ce9ea8635f44491091365c7c2
sahaj-b <sahajb0606@gmail.com>
2025-08-02 13:32:34 +0530

    sexy icon branding

--- manifest.ts
+++ manifest.ts
@@ -4,6 +4,12 @@ export default defineManifest({
   manifest_version: 3,
   name: "BingeMeter",
   version: "1.0.0",
+  icons: {
+    "16": "icon-16.png",
+    "32": "icon-32.png",
+    "48": "icon-48.png",
+    "128": "icon-128.png",
+  },
   options_page: "src/apps/settings/index.html",
   background: {
     service_worker: "src/core/background/index.ts",
@@ -12,6 +18,12 @@ export default defineManifest({
   },
   action: {
     default_popup: "src/apps/popup/index.html",
+    default_icon: {
+      "16": "icon-16.png",
+      "32": "icon-32.png",
+      "48": "icon-48.png",
+      "128": "icon-128.png",
+    },
   },
   optional_host_permissions: ["*://*/*"],
   permissions: ["alarms", "storage", "tabs", "scripting"],
