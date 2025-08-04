79df2ec24fae6196104af23e259dd4df94a588ae
sahaj-b <sahajb0606@gmail.com>
2025-08-04 22:47:02 +0530

    add build:firefox cmd

--- vite.config.ts
+++ vite.config.ts
@@ -7,37 +7,40 @@ import zip from "vite-plugin-zip-pack";
 import path from "node:path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+export default defineConfig(() => {
+  const isFirefox = !!process.env.FIREFOX;
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+        },
+      }),
+      tailwindcss(),
+      crx({
+        manifest,
+        browser: isFirefox ? "firefox" : "chrome",
+      }),
+      zip({ outDir: "release", outFileName: "binge-meter.zip" }),
+    ],
+    build: {
+      rollupOptions: {
+        input: {
+          analytics: "./src/apps/analytics/index.html",
+        },
       },
-    }),
-    tailwindcss(),
-    crx({
-      manifest,
-      // browser: "firefox", // for firefox build
-    }),
-    zip({ outDir: "release", outFileName: "binge-meter.zip" }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        analytics: "./src/apps/analytics/index.html",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@ui": path.resolve(__dirname, "./src/apps/components/ui"),
+        "@lib": path.resolve(__dirname, "./src/apps/lib"),
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@ui": path.resolve(__dirname, "./src/apps/components/ui"),
-      "@lib": path.resolve(__dirname, "./src/apps/lib"),
+    server: {
+      cors: {
+        origin: [/chrome-extension:\/\//],
+      },
     },
-  },
-  server: {
-    cors: {
-      origin: [/chrome-extension:\/\//],
-    },
-  },
+  };
 });
